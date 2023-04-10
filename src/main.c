5e650e09f10badea636e55cfe966a8a5c726ed2a
WinterCore <hogobbl@gmail.com>
2023-04-10 05:46:04 +0200

    fix: Reuse frame buffer instead of creating a new one on every single frame

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <errno.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
 #include "xdg-shell-client-protocol.h"
 
@@ -23,6 +24,15 @@ typedef struct state {
     struct wl_surface *wl_surface;
     struct xdg_surface *xdg_surface;
     struct xdg_toplevel *xdg_toplevel;
+    
+    uint8_t *pix_data;
+    struct wl_buffer *buffer;
+    size_t width;
+    size_t height;
+    size_t stride;
+
+
+    bool is_closed;
 } State;
 
 static void randname(char *buf) {
@@ -113,51 +123,55 @@ static const struct wl_buffer_listener wl_buffer_listener = {
     .release = wl_buffer_release,
 };
 
-static struct wl_buffer *draw_frame(State *state) {
-    const int width = 1920, height = 1080;
-    const int stride = width * 4;
-    const int size = height * stride;
+void draw(State *state) {
+    for (size_t y = 0; y < state->height; y += 1) {
+        for (size_t x = 0; x < state->width; x += 1) {
+            size_t offset = x * 4;
+
+            state->pix_data[y * state->stride + offset + 0] = 0x00; // B
+            state->pix_data[y * state->stride + offset + 1] = 0xFF; // G
+            state->pix_data[y * state->stride + offset + 2] = 0x00; // R
+            state->pix_data[y * state->stride + offset + 3] = 0x30; // Alpha
+        }
+    }
+
+    wl_surface_attach(state->wl_surface, state->buffer, 0, 0);
+    wl_surface_damage_buffer(state->wl_surface, 0, 0, state->width, state->height);
+    wl_surface_commit(state->wl_surface);
+}
+
+void resize(State *state) {
+    const size_t size = state->height * state->stride;
 
     int fd = allocate_shm_file(size);
 
     if (fd == -1) {
-        return NULL;
+        fprintf(stderr, "[ERROR]: allocate_shm_file failed");
+        return;
     }
     
-    uint8_t *data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("\n[INFO(resize)]: SIZE: %ld", size);
+    fflush(stdout);
+    state->pix_data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-    if (data == MAP_FAILED) {
+    if (state->pix_data == MAP_FAILED) {
+        fprintf(stderr, "\n[ERROR]: mmap failed %d", fd);
+        perror("\n");
         close(fd);
-        return NULL;
+        return;
     }
 
     struct wl_shm_pool *pool = wl_shm_create_pool(state->wl_shm, fd, size);
-    struct wl_buffer *buffer = wl_shm_pool_create_buffer(
+    state->buffer = wl_shm_pool_create_buffer(
         pool,
         0,
-        width,
-        height,
-        stride,
+        state->width,
+        state->height,
+        state->stride,
         WL_SHM_FORMAT_ARGB8888
     );
     wl_shm_pool_destroy(pool);
     close(fd);
-
-    for (int y = 0; y < height; y += 1) {
-        for (int x = 0; x < width; x += 1) {
-            int offset = x * 4;
-            data[y * stride + offset + 0] = 0x00; // B
-            data[y * stride + offset + 1] = 0xFF; // G
-            data[y * stride + offset + 2] = 0x00; // R
-            data[y * stride + offset + 3] = 0x30; // Alpha
-        }
-    }
-
-    // munmap(data, size);
-
-    wl_buffer_add_listener(buffer, &wl_buffer_listener, NULL);
-
-    return buffer;
 }
 
 static void xdg_surface_configure(
@@ -167,22 +181,19 @@ static void xdg_surface_configure(
 ) {
     State *state = data;
     xdg_surface_ack_configure(xdg_surface, serial);
-    struct wl_buffer *buffer = draw_frame(state);
-    wl_surface_attach(state->wl_surface, buffer, 0, 0);
-    wl_surface_commit(state->wl_surface);
+
+    printf("\n[INFO(xdg_surface_configure)]\n");
+    fflush(stdout);
+    if (! state->pix_data) {
+        resize(state);
+    }
+
+    draw(state);
 }
 
 static const struct xdg_surface_listener xdg_surface_listener = {
     .configure = xdg_surface_configure,
 };
-
-static void xdg_wm_base_ping(
-    void *data,
-    struct xdg_wm_base *xdg_wm_base,
-    uint32_t serial
-) {
-    xdg_wm_base_pong(xdg_wm_base, serial);
-}
 
 static void wl_surface_frame_done(void *data, struct wl_callback *cb, uint32_t time);
 
@@ -200,15 +211,50 @@ static void wl_surface_frame_done(
     cb = wl_surface_frame(state->wl_surface);
     wl_callback_add_listener(cb, &wl_surface_frame_listener, state);
 
-    struct wl_buffer *buffer = draw_frame(state);
-    wl_surface_attach(state->wl_surface, buffer, 0, 0);
-    wl_surface_damage_buffer(state->wl_surface, 0, 0, INT32_MAX, INT32_MAX);
-    wl_surface_commit(state->wl_surface);
+    draw(state);
 }
 
+void toplevel_configure(
+    void *data,
+    struct xdg_toplevel *top,
+    int32_t nw,
+    int32_t nh, struct wl_array *stat
+) {
+    if (! nw && ! nh) {
+        return;
+    }
+    
+    State *state = data;
+    if (state->width != nw || state->height != nh) {
+        munmap(state->pix_data, state->stride * state->height);
+        state->width = nw;
+        state->height = nh;
+        state->stride = nw * 4;
+        printf("\n[INFO(resize)]: width: %d, height: %d", nw, nh);
+        fflush(stdout);
+
+        resize(state);
+    }
+}
+
+void toplevel_close(void *data, struct xdg_toplevel *xdg_toplevel) {
+    State *state = data;
+    state->is_closed = true;
+}
+
+struct xdg_toplevel_listener toplevel_listener = {
+    .close = toplevel_close,
+    .configure = toplevel_configure,
+};
 
 int main() {
-    State state;
+    State state = {
+        .is_closed = false,
+        .width = 1367,
+        .height = 768,
+        .stride = 1367 * 4,
+    };
+
     state.wl_display = wl_display_connect(NULL);
 
     if (! state.wl_display) {
@@ -220,28 +266,26 @@ int main() {
     wl_registry_add_listener(state.wl_registry, &registry_listener, &state);
     wl_display_roundtrip(state.wl_display);
 
-
     state.wl_surface = wl_compositor_create_surface(state.wl_compositor);
-    state.xdg_surface = xdg_wm_base_get_xdg_surface(state.xdg_wm_base, state.wl_surface);
-    xdg_surface_add_listener(state.xdg_surface, &xdg_surface_listener, &state);
-    state.xdg_toplevel = xdg_surface_get_toplevel(state.xdg_surface);
-    xdg_toplevel_set_title(state.xdg_toplevel, "Wayland Pepega");
-    wl_surface_commit(state.wl_surface);
-
     struct wl_callback *cb = wl_surface_frame(state.wl_surface);
     wl_callback_add_listener(cb, &wl_surface_frame_listener, &state);
 
-    while (wl_display_dispatch(state.wl_display)) {
+    state.xdg_surface = xdg_wm_base_get_xdg_surface(state.xdg_wm_base, state.wl_surface);
+    xdg_surface_add_listener(state.xdg_surface, &xdg_surface_listener, &state);
+    state.xdg_toplevel = xdg_surface_get_toplevel(state.xdg_surface);
+    xdg_toplevel_add_listener(state.xdg_toplevel, &toplevel_listener, &state);
+    xdg_toplevel_set_title(state.xdg_toplevel, "Wayland Pepega");
+    wl_surface_commit(state.wl_surface);
+
+    while (wl_display_dispatch(state.wl_display) != -1) {
+        if (state.is_closed) {
+            break;
+        }
     }
 
-    printf("Connection established!");
-
-    /*
-    while (wl_display_dispatch(display) != -1) {
-    }
-    */
-
-
+    xdg_toplevel_destroy(state.xdg_toplevel);
+    xdg_surface_destroy(state.xdg_surface);
+    wl_surface_destroy(state.wl_surface);
     wl_display_disconnect(state.wl_display);
     
     return 0;
